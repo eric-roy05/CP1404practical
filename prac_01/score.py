fbd2d631ece69838735d874406c79e88e6183e15
eroy1 <eroy10101@gmail.com>
2023-09-25 12:43:11 +1000

    changed name of program

--- prac_01/score.py
+++ prac_01/score.py
@@ -1,14 +1,13 @@
 
 
 score = float(input("Enter score: "))
-if score < 0:
+while score < 0 or score > 100 :
     print("Invalid score")
-else:
-    if score > 100:
-        print("Invalid score")
-    elif score >= 90:
+    score = float(input("Enter score: "))
+
+if score >= 90:
         print("Excellent")
-    elif score >= 50:
+elif score >= 50:
         print("Passable")
-    else:
+else:
         print("Bad")
