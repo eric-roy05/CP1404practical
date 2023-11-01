e70c498502c6fcf2b5eff03234feb5fada80313d
eroy1 <eroy10101@gmail.com>
2023-11-01 14:04:56 +1000

    Recommiting practicals

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -17,7 +17,7 @@ class ProgrammingLanguage:
         return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
 
 
-if __name__ == "__main__":
+def main():
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
@@ -32,3 +32,6 @@ if __name__ == "__main__":
     for language in languages_list:
         if language.is_dynamic():
             print(language.name)
+
+
+main()
